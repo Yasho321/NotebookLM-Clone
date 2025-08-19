9efa4042b29a3b31bf92c5fb78c338a78294d8de
Yasho321 <yashovardhans321@gmail.com>
2025-08-19 17:56:36 +0530

    Auth Setup Done

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import db from './utils/db.js';
-
+import authRoutes from './routes/user.routes.js'
 
 dotenv.config()
 
@@ -28,6 +28,7 @@ app.use(cors({
 app.use(cookieParser())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/api/v1/auth",authRoutes );
 
 
 
