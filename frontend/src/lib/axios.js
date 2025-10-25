ccd84102a6de7fc3ab3d58288260adb8355be759
Yasho321 <159688179+Yasho321@users.noreply.github.com>
2025-10-26 00:50:17 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://notebooklm-clone-m9l4.onrender.com/api/v1';
+const BASE_URL = 'https://notebooklm-clone-rbha.onrender.com/api/v1';
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
